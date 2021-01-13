9ec1e4ba47993d5b8f4f3b84cfa3db9664b9f335
douyonghou <douyonghou@bbdservice.com>
2021-01-13 18:48:48 +0800

    使用maven-assembly-plugin

---
+++ src/test/scala/Test1.scala
@@ -0,0 +1,14 @@
+class Test1 {
+  def apply() = println("I is apply from class")
+}
+
+object Test1 {
+  def apply() = println("I is apply from object")
+
+  def main(args: Array[String]): Unit = {
+    val testClass = new Test1()
+//    testClass()
+    val testObject = Test1()
+//    testObject
+  }
+}
